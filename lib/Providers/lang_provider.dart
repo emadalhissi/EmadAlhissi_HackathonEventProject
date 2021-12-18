537e5535a40e6b480a576ab423f3450ae8126d5c
momen-sisalem92 <momen.sisalem@gmail.com>
2021-12-18 10:49:30 +0200

    first commit

---
+++ lib/Providers/lang_provider.dart
@@ -0,0 +1,24 @@
+import 'package:emad_alhissi_hackathon/shared_preferences/shared_preferences_controller.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class LangProvider extends ChangeNotifier {
+
+  // String lang = SharedPreferencesController().checkLanguage;
+  //
+  // void changeLang(String language) {
+  //   // lang = language;
+  //   SharedPreferencesController().setLanguage(language: language);
+  //   notifyListeners();
+  // }
+
+  String lang = SharedPreferencesController().checkLanguage;
+
+  void changeLang() {
+    lang = lang == 'en' ? 'ar' : 'en';
+    SharedPreferencesController().setLanguage(language: lang);
+    notifyListeners();
+  }
+
+}
